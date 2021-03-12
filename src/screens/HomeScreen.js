f5125827b4867275f86bb99a187ceb3ab334022c
kennyvito10 <43334323+kennyvito10@users.noreply.github.com>
2021-03-12 15:30:03 +0700

    update test 2

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { Text, StyleSheet, View , Button, TouchableOpacity } from "react-native"
 const HomeScreen = ({navigation}) => {
   return( 
   <View> 
-    <Text style={styles.text}>Testss</Text>
+    <Text style={styles.text}>Mantap</Text>
     <Button 
     title="Go to Components Demo" 
     onPress={() => navigation.navigate('Components')}
